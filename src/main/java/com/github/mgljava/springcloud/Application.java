9e2e90f78a4fd18c3537abd92d4dde2f7d1be85e
Minggl <guangliang1120@gmail.com>
2020-01-21 10:32:10 +0800

    [TECH][Minggl]: application配置文件

--- src/main/java/com/github/mgljava/springcloud/Application.java
+++ src/main/java/com/github/mgljava/springcloud/Application.java
@@ -1,9 +1,14 @@
 package com.github.mgljava.springcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
